48f50a368d8fd8aecf3a8f3cdc0ca54709b26113
Yashwin Gokavarapu <Yashwin@Yashwins-MacBook-Pro.local>
2015-04-15 09:19:56 -0400

    First commit

---
+++ src/capturetweet.py
@@ -0,0 +1,51 @@
+'''
+Created on Apr 13, 2015
+
+@author: Yashwin
+'''
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+import sqlite3
+import json
+import simplejson
+#Variables that contains the user credentials to access Twitter API 
+access_token = "3161940045-X4UXjigGRUSpBzoZt5cxDu1Dm6HiQOGGD6uKrRK"
+access_token_secret ="POYCzk2hzjNBktcbyxH6s9vesQrDgwixJtvHufJ8HsgeA"
+consumer_key = "vWI2PWxgJPEwmW72SyBQY06uh"
+consumer_secret = "ab4XsJbncDUZrhpeHzYyfR0cDNTQK9dMXc8VTi163GXnCQSD6I"
+
+
+#This is a basic listener that just prints received tweets to stdout.
+class StdOutListener(StreamListener):
+
+    def on_data(self, data):
+        data1 = json.loads(data)
+        print data1
+        #f= open('data1.txt','a')
+        #f.write(data)
+        with open('data1.json', 'a') as outfile:
+            json.dump(data1, outfile, indent=4)
+        tweets_data = []
+        tweets_file = open('data1.txt', "r")
+        for line in tweets_file:
+            try:
+                tweet = json.loads(line)
+                tweets_data.append(tweet)
+            except:
+                continue
+    def on_error(self, status):
+        print (status)
+    
+    
+
+if __name__ == '__main__':
+
+    #This handles Twitter authetification and the connection to Twitter Streaming API
+    l = StdOutListener()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    stream = Stream(auth, l)
+
+    #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
+    stream.filter(track=['python', 'javascript', 'ruby'])
---
+++ src/dataanalysis.py
@@ -0,0 +1,80 @@
+import json
+import pandas as pd
+import matplotlib.pyplot as plt
+import re
+tweets_data = []
+tweets_file = open('data1.txt', "r")
+for line in tweets_file:
+    try:
+        tweet = json.loads(line)
+        tweets_data.append(tweet)
+    except:
+        continue
+print len(tweets_data)
+tweets = pd.DataFrame()
+
+tweets['text'] = map(lambda tweet: tweet['text'], tweets_data)
+tweets['lang'] = map(lambda tweet: tweet['lang'], tweets_data)
+tweets['country'] = map(lambda tweet: tweet['place']['country'] if tweet['place'] != None else None, tweets_data)
+
+print tweets
+
+tweets_by_lang = tweets['lang'].value_counts()
+
+fig, ax = plt.subplots()
+ax.tick_params(axis='x', labelsize=15)
+ax.tick_params(axis='y', labelsize=10)
+ax.set_xlabel('Languages', fontsize=15)
+ax.set_ylabel('Number of tweets' , fontsize=15)
+ax.set_title('Top 5 languages', fontsize=15, fontweight='bold')
+tweets_by_lang[:5].plot(ax=ax, kind='bar', color='red')
+
+
+tweets_by_country = tweets['country'].value_counts()
+
+fig, ax = plt.subplots()
+ax.tick_params(axis='x', labelsize=15)
+ax.tick_params(axis='y', labelsize=10)
+ax.set_xlabel('Countries', fontsize=15)
+ax.set_ylabel('Number of tweets' , fontsize=15)
+ax.set_title('Top 5 countries', fontsize=15, fontweight='bold')
+tweets_by_country[:5].plot(ax=ax, kind='bar', color='blue')
+
+
+
+
+#data mining
+
+def word_in_text(word, text):
+    word = word.lower()
+    text = text.lower()
+    match = re.search(word, text)
+    if match:
+        return True
+    return False
+
+tweets['python'] = tweets['text'].apply(lambda tweet: word_in_text('python', tweet))
+tweets['javascript'] = tweets['text'].apply(lambda tweet: word_in_text('javascript', tweet))
+tweets['ruby'] = tweets['text'].apply(lambda tweet: word_in_text('ruby', tweet))
+
+print tweets['python'].value_counts()[True]
+print tweets['javascript'].value_counts()[True]
+print tweets['ruby'].value_counts()[True]
+
+prg_langs = ['python', 'javascript', 'ruby']
+tweets_by_prg_lang = [tweets['python'].value_counts()[True], tweets['javascript'].value_counts()[True], tweets['ruby'].value_counts()[True]]
+
+x_pos = list(range(len(prg_langs)))
+width = 0.8
+fig, ax = plt.subplots()
+plt.bar(x_pos, tweets_by_prg_lang, width, alpha=1, color='g')
+
+# Setting axis labels and ticks
+ax.set_ylabel('Number of tweets', fontsize=15)
+ax.set_title('Ranking: python vs. javascript vs. ruby (Raw data)', fontsize=10, fontweight='bold')
+ax.set_xticks([p + 0.4 * width for p in x_pos])
+ax.set_xticklabels(prg_langs)
+plt.grid()
+plt.show()
+
+#extract relevent linkS
